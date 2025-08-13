e82dab140fc813d2eb5343d55ac3a510ef80bc25
rdboyes <randy.boyes@gmail.com>
2025-08-13 11:16:52 -0400

    readme updates

--- R/ai.R
+++ R/ai.R
@@ -8,9 +8,13 @@
 ai <- function(){
   chat <- ellmer::chat_ollama(model = "gpt-oss:20B")
   x <- rlang::last_error()
+
+  pkgs <- tryCatch(paste0(.packages(), collapse = ", "), error =\(x){" no "})
+  objs <- tryCatch(paste0(ls(), collapse = ", "), error = \(x){" [none]"})
+
   chat$chat(glue::glue("I ran {x$call[1]} and got the error {x$message[1]}. 
-  I have {paste0(.packages(), collapse = ", ")} loaded currently. 
-  I have this list of objects defined: {paste0(ls(), collapse = ", ")}.
+  I have {pkgs} loaded currently. 
+  I have this list of objects defined: {objs}.
   Can you help me fix this? I'm using R and I need a quick solution, 
   just one or two paragraphs."))
 }
